a57d3a91ff6c4a6a78060b8a2d422b9afca2c308
Andy Bulka <abulka@gmail.com>
2016-04-03 17:54:57 +1000

    added fix in settings.py, to allow debug_toolbar to work when running in debug mode, by adding reference to static etc

--- settings.py
+++ settings.py
@@ -121,6 +121,8 @@ if DEBUG:
         import debug_toolbar
         MIDDLEWARE_CLASSES.append('debug_toolbar.middleware.DebugToolbarMiddleware')
         INSTALLED_APPS.append('debug_toolbar')
+        INSTALLED_APPS.append('django.contrib.staticfiles')
+        STATIC_URL = '/static/'
     except:
         pass
 
